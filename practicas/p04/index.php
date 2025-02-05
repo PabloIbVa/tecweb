1dbc72a28ee71cbd2f4a31fa900a30e6a3a191be
PabloIbVa <pabloibva@gmail.com>
2025-02-04 22:30:25 -0600

    Se realizo el ejercicio 3 donde se creo distintas variables

--- practicas/p04/index.php
+++ practicas/p04/index.php
@@ -30,5 +30,63 @@
         echo '<li>$house*5 es inválida porque el símbolo * no está permitido.</li>';
         echo '</ul>';
     ?>
+    <h2>Ejercicio 2</h2>
+    <p>Proporcionar los valores de $a, $b, $c como sigue:</p>
+    <p>
+        $a = “ManejadorSQL”;
+        $b = 'MySQL';
+        $c = &$a;
+    </p>
+    <?php
+        //AQUI VA MI CÓDIGO PHP
+        $a = "ManejadorSQL";
+        $b = 'MySQL';
+        $c = &$a;
+        
+        //a) Contenido de variables
+        echo "<p>mostramos contenido de variables</p>";
+        echo "$a, <br>";
+        echo "$b, <br>";
+        echo "$c, <br> <br>";
+
+        //b) Asignaciones
+        $a = "Php server";
+        $b = &$a;
+
+        //c) Volvemos a mostrar contenido de variables
+        echo "<p>mostramos contenido de variables</p>";
+        echo "$a, <br>";
+        echo "$b, <br>";
+        echo "$c, <br> <br>";
+
+        //d) Describe en y muestra en la página obtenida qué ocurrió en el segundo bloque de asignaciones
+        echo "<p> Respuesta: </p>";
+        echo "En el segundo bloque se escribe php server dado a que b y c son apuntadores de a y todos estos apuntan a un solo valor (php server)";
+    ?>
+    <h2>Ejercicio 3</h2>
+    <p>Muestra el contenido de cada variable inmediatamente después de cada asignación,
+       verificar la evolución del tipo de estas variables (imprime todos los componentes de los
+       arreglo):
+    </p>
+    <?php
+        //AQUI VA MI CÓDIGO PHP
+        $a = "PHP5";
+        echo "Primer valor:  $a <br>";
+        $z[] = &$a;
+        echo "Segundo valor: ";
+        print_r($z);
+        echo "<br>";
+        $b = "5a version de PHP";
+        echo "Tercer valor: $b <br>";
+        $c = $b*10;
+        echo "Cuarto valor: $c <br>";
+        $a .= $b;
+        echo "Quinto valor: $a <br>";
+        $b *= $c;
+        echo "Sexto valor: $b <br>";
+        $z[0] = "MySQL";
+        echo "Séptimo valor: , ";
+        print_r($z);
+    ?>
 </body>
 </html>
