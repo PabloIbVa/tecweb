e0c41e9b55e48770b46e8365dd0055ebaf87659b
PabloIbVa <pabloibva@gmail.com>
2025-02-05 14:14:11 -0600

    Se agrego los una nueva carpeta src donde agregaremos las funciones de cada ejercicio de la practica 6 y modificamos el archivo index.php para que reciba las funciones

--- practicas/p06/index.php
+++ practicas/p06/index.php
@@ -11,15 +11,8 @@
     <?php
         if(isset($_GET['numero']))
         {
-            $num = $_GET['numero'];
-            if ($num%5==0 && $num%7==0)
-            {
-                echo '<h3>R= El número '.$num.' SÍ es múltiplo de 5 y 7.</h3>';
-            }
-            else
-            {
-                echo '<h3>R= El número '.$num.' NO es múltiplo de 5 y 7.</h3>';
-            }
+            require_once __DIR__ . '/src/funciones.php';
+            multiplos5y7($_GET['numero']);
         }
     ?>
 
