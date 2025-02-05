fa7b5e4424ef09c53dd4f6e6170a57fdb605462a
PabloIbVa <pabloibva@gmail.com>
2025-02-05 14:51:27 -0600

    Se realizo el ejericicio 2 y se anadio dentro de el documento principal

--- practicas/p06/index.php
+++ practicas/p06/index.php
@@ -15,7 +15,34 @@
             multiplos5y7($_GET['numero']);
         }
     ?>
+    <h2>Ejercicio 2</h2>
+    <p>Crea un programa para la generación repetitiva de 3 números aleatorios hasta obtener una
+    secuencia compuesta por: par,impar,par</p>
+    <?php
+        require_once __DIR__ . '/src/funciones.php';
+        $resul = matriz_3();
+        $matriz = $resul['matriz'];
+        $iter = $resul['iter'];
+        $num = $resul['num'];
+        echo '<h3>Matriz generada:</h3>';
+        echo '<table border="1">';
+        echo '<tr>';
+        echo '<th>Columna 1</th>';
+        echo '<th>Columna 2</th>';
+        echo '<th>Columna 3</th>';
+        echo '</tr>';   
+        for($i=0;$i<$iter;$i++)
+        {
+            echo '<tr>';
+            echo '<td>'.$matriz[$i][0].'</td>';
+            echo '<td>'.$matriz[$i][1].'</td>';
+            echo '<td>'.$matriz[$i][2].'</td>';
+            echo '</tr>';
+        }
+        echo '</table>';
+        echo '<h3>Se creao un total de '.$num.' numeros  y de '.$iter.' Interacciones</h3>';
 
+    ?>
     <h2>Ejemplo de POST</h2>
     <form action="http://localhost/tecweb/practicas/p04/index.php" method="post">
         Name: <input type="text" name="name"><br>
--- practicas/p06/src/funciones.php
+++ practicas/p06/src/funciones.php
@@ -10,4 +10,34 @@
             echo '<h3>R= El número '.$num.' NO es múltiplo de 5 y 7.</h3>';
         }
     }
+    function par_im_par($col1,$col2,$col3){
+        if ($col1%2==0 && $col2%2!=0 && $col3%2==0) {
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    function matriz_3(){
+        $matriz = array();
+        $num = 0;
+        $iter = 0;
+        $i = 0;
+        do{
+            $col1 = rand(1, 100);
+            $col2 = rand(1, 100);
+            $col3 = rand(1, 100);
+            $matriz[$i][0] = $col1;
+            $matriz[$i][1] = $col2;
+            $matriz[$i][2] = $col3;
+            $i++;   
+            $num = $num + 3;
+            $iter++;
+        }while(par_im_par($col1,$col2,$col3)==false);
+        return[
+            'matriz' => $matriz,
+            'iter' => $iter,
+            'num' => $num
+        ];
+    }
 ?>
