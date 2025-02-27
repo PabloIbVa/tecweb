3827bb64cdec2c407cc5c2e641bb06de3e2711d1
PabloIbVa <pabloibva@gmail.com>
2025-02-27 10:02:29 -0600

    se realizo la adaptacion a la lista de productos vigentes para agregar un boton que modifique los datos

--- practicas/p09/formulario_productos_v2.php
+++ practicas/p09/formulario_productos_v2.php
@@ -1,42 +1,92 @@
-<!DOCTYPE html >
+<?php
+// Conectar a la base de datos
+$link = mysqli_connect("localhost", "root", "W0lverine", "marketzone");
+
+// Verificar conexión
+if (!$link) {
+    die("Error: No se pudo conectar a la base de datos. " . mysqli_connect_error());
+}
+
+// Verificar si el formulario ha sido enviado
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['submit'])) {
+    // Obtener datos del formulario
+    $id = mysqli_real_escape_string($link, $_POST['id']);
+    $nombre = mysqli_real_escape_string($link, $_POST['name']);
+    $marca = mysqli_real_escape_string($link, $_POST['marca']);
+    $modelo = mysqli_real_escape_string($link, $_POST['model']);
+    $precio = mysqli_real_escape_string($link, $_POST['price']);
+    $detalle = mysqli_real_escape_string($link, $_POST['detail']);
+    $cantidad = mysqli_real_escape_string($link, $_POST['cant']);
+    $imagen = mysqli_real_escape_string($link, $_POST['img']);
+
+    // Consulta para actualizar
+    $sql = "UPDATE productos 
+          SET nombre='$nombre', 
+          marca='$marca', 
+          modelo='$modelo', 
+          precio=$precio, 
+          detalles='$detalle', 
+          unidades=$cantidad, 
+          imagenes='$imagen' 
+          WHERE id=$id;";
+
+    if (mysqli_query($link, $sql)) {
+        // Redirigir después de actualizar
+        header("Location: http://localhost/tecweb/practicas/p09/get_productos_vigentes_v2.php");
+        exit();
+    } else {
+        echo "<script>alert('Error al actualizar: " . mysqli_error($link) . "');</script>";
+    }
+}
+
+// Cerrar conexión
+mysqli_close($link);
+?>
+
+<!DOCTYPE html>
 <html>
 
-  <head>
-    <meta charset="utf-8" >
-    <title>Registros para Productos</title>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <style type="text/css">
-      ol, ul { 
-      list-style-type: none;
-      }
+        ol,
+        ul {
+            list-style-type: none;
+        }
     </style>
-  </head>
+    <title>Formulario</title>
+</head>
 
-  <body>
-    <h1>Insertacion de productos nuevos</h1>
+<body>
+    <h1>Modificacion de Producto</h1>
     <br>
-    <form id="formularioTenis" action="http://localhost/tecweb/practicas/p08/src/set_producto_v2.php" method="post">
+    <form id="miFormulario" action="" method="post">
 
-    <h2>Detalles de Producto a Insertar</h2>
+        <h2>Detalles de Producto a Insertar</h2>
 
-      <fieldset>
-        <legend></legend>
+        <fieldset>
+            <legend>Actualizacion de datos:</legend>
 
-        <ul>
-          <li><label for="form-name">Nombre:</label> <input type="text" name="name" id="form-name"></li>
-          <li><label for="form-marca">Marca:</label> <input type="text" name="marca" id="form-marca"></li>
-          <li><label for="form-model">Modelo:</label> <input type="text" name="model" id="form-model"></li>
-          <li><label for="form-price">Precio:</label> <input type="text" name="price" id="form-price"></li>
-          <li><label for="form-detail">Detalles</label><br><textarea name="detail" rows="4" cols="60" id="form-detail" placeholder="Escribe características del producto"></textarea></li>
-          <li><label for="form-cant">Unidades:</label> <input type="number" name="cant" min="0" id="form-cant"></li>
-          <li><label for="form-img">Imagen:</label> <input type="text" name="img" id="form-img"></li>
-        </ul>
-      </fieldset>
+            <ul>
+                <li><label for="form-id">ID:</label> <input type="text" name="id" id="form-id" value="<?= !empty($_POST['ids']) ? $_POST['ids'] : $_GET['ids'] ?>" readonly></li>
+                <li><label for="form-name">Nombre:</label> <input type="text" name="name" id="form-name" value="<?= !empty($_POST['nombre']) ? $_POST['nombre'] : $_GET['nombre'] ?>"></li>
+                <li><label for="form-marca">Marca:</label> <input type="text" name="marca" id="form-marca" value="<?= !empty($_POST['marca']) ? $_POST['marca'] : $_GET['marca'] ?>"></li>
+                <li><label for="form-model">Modelo:</label> <input type="text" name="model" id="form-model" value="<?= !empty($_POST['modelo']) ? $_POST['modelo'] : $_GET['modelo'] ?>"></li>
+                <li><label for="form-price">Precio:</label> <input type="text" name="price" id="form-price" value="<?= !empty($_POST['precio']) ? $_POST['precio'] : $_GET['precio'] ?>"></li>
+                <li><label for="form-detail">Detalles</label><br><textarea name="detail" rows="4" cols="60" id="form-detail" placeholder="Escribe características del producto"><?= !empty($_POST['detalles']) ? $_POST['detalles'] : $_GET['detalles'] ?></textarea></li>
+                <li><label for="form-cant">Unidades:</label> <input type="number" name="cant" min="0" id="form-cant" value="<?= !empty($_POST['cantidad']) ? $_POST['cantidad'] : $_GET['cantidad'] ?>"></li>
+                <li><label for="form-img">Imagen:</label> <input type="text" name="img" id="form-img" value="<?= !empty($_POST['imagen']) ? $_POST['imagen'] : $_GET['imagen'] ?>"></li>
+            </ul>
+        </fieldset>
 
-      <p>
-        <input type="submit" value="Subir producto">
-        <input type="reset">
-      </p>
+        <p>
+            <input type="submit" name="submit" value="Subir producto">
+            <input type="reset">
+        </p>
 
     </form>
-  </body>
+</body>
+
 </html>
--- practicas/p09/get_productos_vigentes_v2.php
+++ practicas/p09/get_productos_vigentes_v2.php
@@ -36,10 +36,99 @@
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
         <title>Producto</title>
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <link 
+            rel="stylesheet" 
+            href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" 
+            integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" 
+            crossorigin="anonymous">
+        <script>
+            function showDetails(event) {
+                var row = event.target.closest("tr"); // Encuentra la fila más cercana dado a que no te e
+                var data = row.querySelectorAll(".row-data");
+
+                var id = data[0].innerText;
+                var name = data[1].innerText;
+                var marca = data[2].innerText;
+                var model = data[3].innerText;
+                var price = data[4].innerText;
+                var cantidad = data[5].innerText;
+                var detail = data[6].innerText;
+                var img = data[7].querySelector("img").getAttribute('src'); // Obtiene la URL de la imagen
+
+                alert("ID: " + id + 
+                    "\nNombre: " + name + 
+                    "\nMarca: "  + marca +
+                    "\nModelo: " + model +
+                    "\nPrecio: " + price +
+                    "\nCantidad: " + cantidad +
+                    "\nDetalles: " + detail +
+                    "\nImagen: " + img);
+
+                sendForm(id, name, marca, model, price, cantidad, detail, img);
+            }
+            function sendForm(id, name, marca, model, price, cantidad, detail, img) {
+                var form = document.createElement("form");
+
+                var idIn = document.createElement("input");
+                idIn.type = 'text';
+                idIn.name = 'ids';
+                idIn.value = id;
+                form.appendChild(idIn);
+
+                var nombreIn = document.createElement("input");
+                nombreIn.type = 'text';
+                nombreIn.name = 'nombre';
+                nombreIn.value = name;
+                form.appendChild(nombreIn);
+
+                var marcaIn = document.createElement("input");
+                marcaIn.type = 'text';
+                marcaIn.name = 'marca';
+                marcaIn.value = marca;
+                form.appendChild(marcaIn);
+
+                var modeloIn = document.createElement("input");
+                modeloIn.type = 'text';
+                modeloIn.name = 'modelo';
+                modeloIn.value = model;
+                form.appendChild(modeloIn);
+
+                var precioIn = document.createElement("input");
+                precioIn.type = 'text';
+                precioIn.name = 'precio';
+                precioIn.value = price;
+                form.appendChild(precioIn);
+
+                var detallesIn = document.createElement("input");
+                detallesIn.type = 'text';
+                detallesIn.name = 'detalles';
+                detallesIn.value = detail;
+                form.appendChild(detallesIn);
+
+                var cantidadIn = document.createElement("input");
+                cantidadIn.type = 'text';
+                cantidadIn.name = 'cantidad';
+                cantidadIn.value = cantidad;
+                form.appendChild(cantidadIn);
+
+                var imagenIn = document.createElement("input");
+                imagenIn.type = 'text';
+                imagenIn.name = 'imagen';
+                imagenIn.value = img;
+                form.appendChild(imagenIn);
+
+                console.log(form);
+
+                form.method = 'POST';
+                form.action = 'formulario_productos_v2.php';  
+
+                document.body.appendChild(form);
+                form.submit();
+            }
+        </script>
     </head>
     <body>
-        <h3>PRODUCTO</h3>
+        <h3>PRODUCTOS</h3>
         
         <?php if (!empty($data)) : ?>
         <table class="table">
@@ -53,19 +142,21 @@
                     <th scope="col">Unidades</th>
                     <th scope="col">Detalles</th>
                     <th scope="col">Imagen</th>
+                    <th scope="col">Datos</th>
                 </tr>
             </thead>
             <tbody>
                 <?php foreach ($data as $reg) : ?>
                     <tr>
-                        <th scope="row"><?= htmlspecialchars($reg['id']) ?></th>
-                        <td><?= htmlspecialchars($reg['nombre']) ?></td>
-                        <td><?= htmlspecialchars($reg['marca']) ?></td>
-                        <td><?= htmlspecialchars($reg['modelo']) ?></td>
-                        <td><?= htmlspecialchars($reg['precio']) ?></td>
-                        <td><?= htmlspecialchars($reg['unidades']) ?></td>
-                        <td><?= htmlspecialchars($reg['detalles']) ?></td>
-                        <td><img src=<?= htmlspecialchars($reg['imagenes']) ?> ></td>
+                        <th scope="row" class="row-data"><?= htmlspecialchars($reg['id']) ?></th>
+                        <td class="row-data"><?= htmlspecialchars($reg['nombre']) ?></td>
+                        <td class="row-data"><?= htmlspecialchars($reg['marca']) ?></td>
+                        <td class="row-data"><?= htmlspecialchars($reg['modelo']) ?></td>
+                        <td class="row-data"><?= htmlspecialchars($reg['precio']) ?></td>
+                        <td class="row-data"><?= htmlspecialchars($reg['unidades']) ?></td>
+                        <td class="row-data"><?= htmlspecialchars($reg['detalles']) ?></td>
+                        <td class="row-data"><img src=<?= htmlspecialchars($reg['imagenes']) ?> ></td>
+                        <td class="row-data"><input type="button" value="Mostrar Datos" onclick="showDetails(event)"></td>
                     </tr>
                 <?php endforeach; ?>
             </tbody>
