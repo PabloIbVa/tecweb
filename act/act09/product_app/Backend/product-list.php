5d826ad9d285c6234fce8efecfb6ec2725d3bffe
PabloIbVa <pabloibva@gmail.com>
2025-04-29 20:07:17 -0600

    Se eliminarion los archivos que son llamados desde el frontend

--- act/act09/product_app/Backend/product-list.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once __DIR__.'/../vendor/autoload.php'; // Autoload de Composer
-use Myapi\Read\Read as Products; // Hacemos que la clase Products esté disponible
-
-$prod = new Products('marketzone');
-$prod->list();
-echo $prod->getData();
-?>
