bedb0921faeeb3956ffe48e2c888279d247f073b
PabloIbVa <pabloibva@gmail.com>
2025-04-29 20:01:31 -0600

    Se creo una copia de la practica 12 para la actividad 9

---
+++ act/act09/product_app/Backend/Myapi/Delete/Delete.php
@@ -0,0 +1,27 @@
+<?php
+namespace Myapi\Delete;
+use Myapi\DataBase;
+class Delete extends DataBase{
+    public function __construct($db,$user='root',$pass='W0lverine'){
+        $this->data = array();
+        parent::__construct($db,$user,$pass);   
+    }
+
+    public function delete($id){
+        $msj = array(
+            'status'  => 'error',
+            'message' => 'La consulta falló'
+        );
+        if( $id ) {
+            $sql = "UPDATE productos SET eliminado=1 WHERE id = {$id}";
+            if ( $this -> conexion->query($sql) ) {
+                $this -> data['status'] =  "success";
+                $this -> data['message'] =  "Producto eliminado";
+            } else {
+                $this -> data['message'] = "ERROR: No se ejecuto $sql. " . mysqli_error($this -> conexion);
+            }
+            $this -> conexion->close();
+        } 
+    }
+}
+?>
---
+++ act/act09/product_app/Backend/product-add.php
@@ -0,0 +1,18 @@
+<?php
+header('Content-Type: application/json'); // Agregar header JSON
+
+require_once __DIR__.'/../vendor/autoload.php'; // Autoload de Composer
+use Myapi\Create\Create as Products; // Hacemos que la clase Products esté disponible
+
+try {
+    $producto = file_get_contents('php://input'); // Obtener el contenido del body
+    $prod = new Products('marketzone'); // Instanciar la clase Products
+    $prod->add($producto); // Llamar al método add
+    echo json_encode($prod->getData()); // Imprimir el resultado
+} catch(Exception $e) { // Manejo de excepciones
+    echo json_encode([ // Imprimir mensaje de error
+        'status' => 'error', //en caso de error
+        'message' => $e->getMessage() //mensaje de error
+    ]);
+}
+?>
